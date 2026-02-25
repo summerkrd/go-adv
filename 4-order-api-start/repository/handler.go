af913564085db678c974a5bb662b96ef0404bb43
Anatoliy <karp.anatol@gmail.com>
2026-02-25 11:43:12 +0300

    доделал хэндлер

--- 4-order-api-start/repository/handler.go
+++ 4-order-api-start/repository/handler.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"go-adv/4-order-api-start/db"
 	"net/http"
+	"strconv"
 )
 
 func NewRepoHandler(router http.ServeMux, db *db.Db) {
 	repo := NewRepository(db)
 	router.HandleFunc("POST /product/create", CreateProduct(repo))
-	router.HandleFunc("POST /product/update/{id}", UpdateProduct(repo))
+	router.HandleFunc("POST /product/update", UpdateProduct(repo))
 	router.HandleFunc("POST /product/delete/{id}", DeleteProduct(repo))
 	router.HandleFunc("GET /product/{id}", GetByID(repo))
 }
@@ -24,18 +25,25 @@ func CreateProduct(repo *Repository) func(w http.ResponseWriter, r *http.Request
 
 func UpdateProduct(repo *Repository) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		product := Product{}
+		json.NewDecoder(r.Body).Decode(&product)
+		repo.Update(&product)
 	}
 }
 
 func DeleteProduct(repo *Repository) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		idStr := r.PathValue("id")
+		idInt, _ := strconv.Atoi(idStr)
+		repo.Delete(uint(idInt))
 	}
 }
 
 func GetByID(repo *Repository) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		idStr := r.PathValue("id")
+		idInt, _ := strconv.Atoi(idStr)
+		product, _ := repo.GetByID(uint(idInt))
+		json.NewEncoder(w).Encode(product)
 	}
 }
