bd739e2d0a8f006e07ce8e26ace02a32596004ca
Anatoliy <karp.anatol@gmail.com>
2026-03-02 10:02:01 +0300

    домашка по логированию

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"go-adv/3-validation-api/server"
 	"go-adv/3-validation-api/verify"
 	"go-adv/4-order-api-start/db"
+	"go-adv/4-order-api-start/middleware"
 	"go-adv/4-order-api-start/repository"
 	"net/http"
 )
@@ -16,5 +17,6 @@ func main() {
 	serv := server.NewServer()
 	serv.RegisterRoutes(verifier)
 	repository.NewRepoHandler(serv.Router, dataBase)
-	http.ListenAndServe(":8081", &serv.Router)
+	handler := http.Handler(middleware.Log(&serv.Router))
+	http.ListenAndServe(":8081", handler)
 }
